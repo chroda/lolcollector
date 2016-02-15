bedd0b16a5edceeed42eb012d38cb0937906b3f7
chroda <chroda@chroda.com.br>
2016-02-15 05:08:46 -0300

    testing Dan videos

--- boilerplate/src/app/Testing.js
+++ boilerplate/src/app/Testing.js
@@ -1,8 +1,35 @@
 import React from 'react';
+import { createStore } from 'redux'
+
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    default:
+      return state
+  }
+}
+
+const store = createStore(counter)
+const render = () => {
+  document.body.innerText = store.getState();
+
+  render(provider, document.getElementById('app'));
+}
+store.subscribe(render);
+
+const incrementOne = () => {
+  store.dispatch({ type: 'INCREMENT' });
+}
 
 const Testing = () => (
   <div>
-    testing
+    the current state is:
+    <br/>
+    <button onClick={incrementOne()} >+</button>
+    <button onClick={store.dispatch({ type: 'DECREMENT' })} >-</button>
   </div>
 );
 
