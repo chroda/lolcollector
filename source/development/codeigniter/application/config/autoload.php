110398e0fd70f1071ffb381bf8699c524239cf83
chroda <chroda@chroda.com.br>
2016-06-26 20:45:21 -0300

    postman collection

--- source/development/codeigniter/application/config/autoload.php
+++ source/development/codeigniter/application/config/autoload.php
@@ -58,7 +58,9 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array();
+$autoload['libraries'] = array(
+
+);
 
 /*
 | -------------------------------------------------------------------
@@ -89,7 +91,10 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array();
+$autoload['helper'] = array(
+  'url',
+  'utils'
+);
 
 /*
 | -------------------------------------------------------------------
--- source/development/codeigniter/application/helpers/utils_helper.php
+++ source/development/codeigniter/application/helpers/utils_helper.php
@@ -1,6 +1,6 @@
 <?php
 
-function pr($expression,$dump=false){
+function pr($expression=false,$dump=false){
 	print'<pre style="z-index:9999;block;position:relative;">';
 	$dump===true?var_dump($expression):print_r($expression);
 	print'</pre>';
