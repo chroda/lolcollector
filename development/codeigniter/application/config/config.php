c8d2947ff531b5db2c460c777974d0e013182923
chroda <chroda@chroda.com.br>
2016-06-30 23:42:32 -0300

    restserver

--- development/codeigniter/application/config/config.php
+++ development/codeigniter/application/config/config.php
@@ -23,7 +23,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = '';
+
+switch ($_SERVER['SERVER_NAME']) {
+  case 'www.lolcollector.com.br': header('location:http://lolcollector.com.br/')                      ;break;
+  case 'lolcollector.com.br'    : $base_url = 'http://lolcollector.com.br/'                           ;break;
+  default                       : $base_url = 'http://localhost/lolcollector/development/codeigniter/';break;
+}
+$config['base_url'] = $base_url;
 
 /*
 |--------------------------------------------------------------------------
@@ -35,7 +41,7 @@ $config['base_url'] = '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
---
+++ development/codeigniter/application/views/welcome.php
@@ -0,0 +1,7 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+
+
+hello word?
+<br/>
+<?php echo anchor(base_url(),'home'); ?>
