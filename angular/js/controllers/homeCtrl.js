542bda9581d826105d7ba811c0365b6680dac8a0
chroda <chroda@chroda.com.br>
2017-06-24 18:29:41 -0300

    NG6 test

--- angular/js/controllers/homeCtrl.js
+++ angular/js/controllers/homeCtrl.js
@@ -2,6 +2,7 @@ app.controller("homeCtrl", function($scope, $location, summonersAPI){
   $scope.summoner;
   $scope.searchSummoner = function(summoner){
     summonersAPI.searchByName(summoner.name).success(function(data){
+      console.log(data);
       $scope.summoner = data;
       $scope.homeForm.$setPristine();
       $location.path("/details");
--- angular/js/services/summonersAPIService.js
+++ angular/js/services/summonersAPIService.js
@@ -2,8 +2,7 @@ app.factory("summonersAPI", function($http, config, riotapi, riotsecret){
 
   return {
     searchByName : function(summoner){
-      console.log(riotapi);
-      return $http.get(riotapi.baseurl+'lol/summoner/v3/summoners/by-name/'+summoner, {params:{api_key:riotsecret}});
+      return $http.get(riotapi.baseurl+'lol/summoner/v3/summoners/by-name/'+summoner+'?api_key='+riotsecret);
     }
 
 
