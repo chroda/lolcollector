038c58424d7f019c33f96dd3476340d92d79ac2e
chroda <chroda@chroda.com.br>
2017-06-25 08:56:37 -0300

    url path in test

--- angular/js/services/summonersAPIService.js
+++ angular/js/services/summonersAPIService.js
@@ -2,7 +2,7 @@ app.factory("summonersAPI", function($http, config, riotapi, riotsecret){
 
   return {
     searchByName : function(summoner){
-      const address = 'http://localhost/lolcollector/slim/public/summonersbyname/chroda';
+      const address = '../slim/public/summonersbyname/chroda';
       // console.log(address);
       // return $http.get(riotapi.baseurl+'lol/summoner/v3/summoners/by-name/'+summoner+'?api_key='+riotsecret);
       return $http.get(address);
