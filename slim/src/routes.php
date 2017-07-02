6b2deca0f2a137ec3aafe670438464c380f082b9
chroda <chroda@chroda.com.br>
2017-07-02 19:47:57 -0300

    endpoint /champion-mastery-by-summoner/[{summonerId}
    Endpoint get the total mastery points of a summoner

--- slim/src/routes.php
+++ slim/src/routes.php
@@ -20,12 +20,11 @@ $app->get('/summonersbyname/[{name}]', function ($request, $response, $args) {
 });
 
 
-$app->get('/champion-mastery-by-summoner/[{name}]', function ($request, $response, $args) {
-  // $this->logger->info("Slim-Skeleton '/' route");
-  // /lol/champion-mastery/v3/scores/by-summoner/{summonerId}
+$app->get('/champion-mastery-by-summoner/[{summonerId}]', function ($request, $response, $args) {
   $args = (object) $args;
-  $requisition = json_decode(file_get_contents('https://br1.api.riotgames.com/lol/summoner/v3/summoners/by-name/'.$args->name.'?api_key=2a0a5c1e-7355-42dc-8e2b-f25d5ee9771f'));
-  return $response->withJson([
+  $endpoint = 'https://'.$this->riot->server.'.'.$this->riot->baseurl.'/lol/champion-mastery/v3/scores/by-summoner/'.$args->summonerId.'?api_key='.$this->riot->key;
+  $this->logger->info('Endpoint: '.$endpoint);
+  $requisition = json_decode(file_get_contents($endpoint));return $response->withJson([
     'code' => 200,
     'data' => $requisition
   ]);
