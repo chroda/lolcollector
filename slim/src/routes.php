aa3b54bbee01c318df5e1f613e8fc8f489e701c6
chroda <chroda@chroda.com.br>
2017-07-11 17:24:44 -0300

    root route

--- slim/src/routes.php
+++ slim/src/routes.php
@@ -2,10 +2,11 @@
 // Routes
 
 $app->get('/', function ($request, $response, $args) {
-  pr($this->riot);
+  echo 'LoLCollector API - lolcollector.chroda.com.br';
 });
 
 $app->get('/summoner-by-name/[{name}]', function ($request, $response, $args) {
+  $r = $this->riot;
   $args = (object) $args;
   $endpoint = 'https://'.$this->riot->server.'.'.$this->riot->baseurl.'/lol/summoner/v3/summoners/by-name/'.$args->name.'?api_key='.$this->riot->key;
   $this->logger->info('Endpoint: '.$endpoint);
@@ -20,7 +21,8 @@ $app->get('/champion-mastery-by-summoner/[{summonerId}]', function ($request, $r
   $args = (object) $args;
   $endpoint = 'https://'.$this->riot->server.'.'.$this->riot->baseurl.'/lol/champion-mastery/v3/scores/by-summoner/'.$args->summonerId.'?api_key='.$this->riot->key;
   $this->logger->info('Endpoint: '.$endpoint);
-  $requisition = json_decode(file_get_contents($endpoint));return $response->withJson([
+  $requisition = json_decode(file_get_contents($endpoint));
+  return $response->withJson([
     'code' => 200,
     'data' => $requisition
   ]);
