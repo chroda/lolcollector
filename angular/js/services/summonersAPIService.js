84108ce8c9b34b22abdde2a4215c30a424123fe0
chroda <chroda@chroda.com.br>
2017-07-13 19:09:44 -0300

    frontend address of service

--- angular/js/services/summonersAPIService.js
+++ angular/js/services/summonersAPIService.js
@@ -3,7 +3,7 @@ app.factory("summonersAPI", function($http, config, riotapi, riotsecret){
   return {
     searchByName : function(summoner){
 
-      const address = '../slim/public/summonersbyname/'+summoner;
+      const address = 'summonersbyname/'+summoner;
       // console.log(address);
       // return $http.get(riotapi.baseurl+'lol/summoner/v3/summoners/by-name/'+summoner+'?api_key='+riotsecret);
       return $http.get(address);
