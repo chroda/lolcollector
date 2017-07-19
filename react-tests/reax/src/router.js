1f8c859541086fdad8824b8e62b2d16965e9696f
chroda <chroda@chroda.com.br>
2017-07-18 22:04:35 -0300

    prod branch

--- react-tests/reax/src/router.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Router } from 'react-router';
-import history from './history.js'
-import routes from './routes.js';
-
-const router = <Router history={history} routes={routes} />;
-
-export default router;
